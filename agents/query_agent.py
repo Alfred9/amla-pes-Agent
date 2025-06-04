b53b3e2ded9958e2f392530fada4999f6c1ea43c
Alfred Isair <163370258+Alfred9@users.noreply.github.com>
2025-06-04 12:21:21 +0300

    Update query_agent.py

--- agents/query_agent.py
+++ agents/query_agent.py
@@ -1,1 +1,12 @@
+rom langchain.prompts import PromptTemplate
+
+query_template = PromptTemplate.from_template(
+    """Given this extracted patient info:
+{info}
+
+Generate a concise PubMed search query string:
+""")
+
+def generate_search_queries(info):
+    return (query_template | ChatOpenAI(temperature=0)).invoke({"info": info})
 
