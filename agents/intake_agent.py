328f9ed21143450feb06f7592e981a6e4d5b6143
Alfred Isair <163370258+Alfred9@users.noreply.github.com>
2025-06-04 12:19:35 +0300

    Update intake_agent.py

--- agents/intake_agent.py
+++ agents/intake_agent.py
@@ -1,1 +1,19 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
 
+llm = ChatOpenAI(temperature=0)
+template = PromptTemplate.from_template(
+    """Extract the following from this clinical note:
+- Age
+- Gender
+- Symptoms
+- Diagnoses
+Return as a JSON.
+
+Note:
+{note}
+""")
+
+def extract_patient_info(note):
+    chain = template | llm
+    return chain.invoke({"note": note})
